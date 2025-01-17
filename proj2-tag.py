b50b87ac52cbf97b14b191db89c123f2de81f619
elisrb <elisrb2004@gmail.com>
2025-01-16 21:43:01 -0300

    emparelhamento estável sem máximo

--- proj2-tag.py
+++ proj2-tag.py
@@ -34,7 +34,8 @@ def read_file(path):
                     preferencia_alunos[aluno].append(p)
                 nota_alunos[aluno] = int(nota)
 
-def emparelhamento_estavel(matches):
+def emparelhamento_estavel(alunos):
+    matches = {projeto: [] for projeto in projetos}
     alunos_livres = alunos
     proximo_pedido = {aluno: 0 for aluno in alunos}
 
@@ -49,16 +50,17 @@ def emparelhamento_estavel(matches):
                     matches[projeto_atual].append(aluno_atual)
 
                 else:
-                    for aluno in matches[projeto_atual]:
+                    for aluno in matches[projeto_atual]:  # fazer recursivo com uma opção para substituir cada item dos alunos no match atual, e maximizar
                         if nota_alunos[aluno] > nota_alunos[aluno_atual]:
                             matches[projeto_atual].remove(aluno)
                             alunos_livres.append(aluno)
                             matches[projeto_atual].append(aluno_atual)
                             break
+
         alunos_livres.pop(0)
-        
+
         matches[projeto_atual].sort(key=lambda a: nota_alunos[a])
-        print(*[nota_alunos[a] for a in matches[projeto_atual]])
+        #print(*[nota_alunos[a] for a in matches[projeto_atual]])
 
     return matches
 
@@ -74,14 +76,15 @@ print(*preferencia_alunos.items())
 print(*nota_alunos.items())
 """
 
-matches = {projeto: [] for projeto in projetos}
-
-matches = emparelhamento_estavel(matches)
-matches = emparelhamento_estavel(matches)
-
+results = []
+matches = emparelhamento_estavel(alunos)
 total = 0
-
 for p, a in matches.items():
-    print(p + ":", *a)
+    #print(p + ":", *a)
     total+=len(a)
+    if len(a) < vagas_projetos[p]:
+        print("UEPA")
 print(total)
+results.append(total)
+
+print(*results)
